76c6b215463101b0ca56d687182e9719315d7c91
Michael Repinskiy <mihailrepinskie9999@icloud.com>
2024-06-29 20:59:16 +0400

    Feature: added starter class

---
+++ src/main/java/com/example/telemedicineSystem/TelemedicineSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.telemedicineSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelemedicineSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelemedicineSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/telemedicineSystem/TelemedicineSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telemedicineSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelemedicineSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
