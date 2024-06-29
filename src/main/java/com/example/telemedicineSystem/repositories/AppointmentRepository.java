68e271a41b1b28fd20f56633685fffbea9142069
Michael Repinskiy <mihailrepinskie9999@icloud.com>
2024-06-29 20:47:56 +0400

    Feature: added new Repositories

---
+++ src/main/java/com/example/telemedicineSystem/repositories/AppointmentRepository.java
@@ -0,0 +1,9 @@
+package com.example.telemedicineSystem.repositories;
+
+import com.example.telemedicineSystem.models.Appointment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+}
