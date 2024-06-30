e5aec5efd8616a3066794ffdf4d8e0d5799ff917
Michael Repinskiy <mihailrepinskie9999@icloud.com>
2024-06-30 21:02:01 +0400

    Feature: added new Exception hendlers

---
+++ src/main/java/com/example/telemedicineSystem/exceptions/AppError.java
@@ -0,0 +1,14 @@
+package com.example.telemedicineSystem.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AppError {
+    private String code;
+    private String value;
+}
+
