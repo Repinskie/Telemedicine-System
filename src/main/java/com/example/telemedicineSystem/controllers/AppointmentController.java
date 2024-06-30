b59fc7d946811459e47feca4ca97ce485b8aeefc
Michael Repinskiy <mihailrepinskie9999@icloud.com>
2024-06-30 20:59:10 +0400

    Feature: added new AuthenticationController

--- src/main/java/com/example/telemedicineSystem/controllers/AppointmentController.java
+++ src/main/java/com/example/telemedicineSystem/controllers/AppointmentController.java
@@ -1,4 +1,30 @@
 package com.example.telemedicineSystem.controllers;
 
+import com.example.telemedicineSystem.dto.AppointmentDto;
+import com.example.telemedicineSystem.services.AppointmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/appointments")
+@RequiredArgsConstructor
 public class AppointmentController {
+    private final AppointmentService appointmentService;
+
+    @GetMapping
+    public List<AppointmentDto> getAllProducts() {
+        return appointmentService.findAll();
+    }
+
+    @PostMapping("/create")
+    public AppointmentDto createAppointment(@RequestBody AppointmentDto appointmentDto) {
+
+    }
+
+    @PutMapping("/appointments/{id}")
+    public AppointmentDto getAppointmentById(@PathVariable int id) {
+
+    }
 }
